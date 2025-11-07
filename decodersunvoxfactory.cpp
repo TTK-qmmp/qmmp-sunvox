b3b2694a02ba27c68b41a493b77cea34293f53ff
Greedysky <greedysky@163.com>
2025-11-07 15:22:39 +0800

    feat: Support for qmmp 0.x Qt4

--- decodersunvoxfactory.cpp
+++ decodersunvoxfactory.cpp
@@ -98,3 +98,8 @@ QString DecoderSunVoxFactory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(sunvox, DecoderSunVoxFactory)
+#endif
